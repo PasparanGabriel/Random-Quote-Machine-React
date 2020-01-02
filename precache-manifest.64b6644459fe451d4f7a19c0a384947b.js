d7115f4efa249e8a3e78287d26511bf71946bae9
PasparanGabriel <pasparangabriel@yahoo.com>
2020-01-02 20:22:00 +0000

    Updates

---
+++ precache-manifest.64b6644459fe451d4f7a19c0a384947b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fc7e08eb207f1b5c9fa",
+    "url": "/Random-Quote-Machine-React/static/css/main.63568edc.chunk.css"
+  },
+  {
+    "revision": "7fc7e08eb207f1b5c9fa",
+    "url": "/Random-Quote-Machine-React/static/js/main.27d069ca.chunk.js"
+  },
+  {
+    "revision": "ade1f4fecc3464434500",
+    "url": "/Random-Quote-Machine-React/static/js/runtime~main.a6520ca0.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/Random-Quote-Machine-React/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "1833afc630e103eb497b049f3db38c96",
+    "url": "/Random-Quote-Machine-React/index.html"
+  }
+];
